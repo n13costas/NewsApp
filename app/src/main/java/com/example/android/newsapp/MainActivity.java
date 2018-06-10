75cf79e4668d74b7281448292fbf4989543c24c5
Konstantinos Tsekos <konstantinos.tsekos@gmail.com>
2018-06-10 23:03:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.newsapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //open the news list
+        TextView startButton = findViewById(R.id.startView);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent showListIntent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(showListIntent);
+            }
+        });
+    }
+}
